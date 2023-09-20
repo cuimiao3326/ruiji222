d9cb27d62db3f8d6d58ea5c7d3d425d32faccaa5
cuimiao <1397220725@qq.coom>
2023-09-20 10:40:14 +0800

    category

--- src/main/java/org/example/ruiji/controller/CategoryController.java
+++ src/main/java/org/example/ruiji/controller/CategoryController.java
@@ -1,7 +1,10 @@
 package org.example.ruiji.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.example.ruiji.common.R;
 import org.example.ruiji.entity.Category;
 import org.example.ruiji.service.ICategoryService;
@@ -28,6 +31,19 @@ public class CategoryController {
         log.info("获得的菜单信息为：{}",category);
         iCategoryService.save(category);
         return R.success("添加菜单信息成功");
+    }
+
+
+    @GetMapping("/page")
+    public R<Page<Category>> getInfo(Integer page,Integer pageSize,String name){
+        //分页构造器
+        Page<Category> categoryPage = new Page<>(page, pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Category> categoryLambdaQueryWrapper = new LambdaQueryWrapper<Category>()
+                .orderByAsc(Category::getSort);
+        //执行查询
+        iCategoryService.page(categoryPage,categoryLambdaQueryWrapper);
+        return R.success(categoryPage);
     }
 
 }
